d1c0457c92eb112c4112340cb66bdde822e2d5cf
Ashish Das <ashish09@cisco.com>
2022-02-11 14:11:02 +0530

    Removed the view from activity

--- cordova-plugin-external-storage-permissions/src/android/cordova/plugin/permissions/ExternalStoragePermissionActivity.java
+++ cordova-plugin-external-storage-permissions/src/android/cordova/plugin/permissions/ExternalStoragePermissionActivity.java
@@ -2,6 +2,7 @@ package cordova.plugin.permissions;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -13,10 +14,11 @@ import org.json.JSONObject;
 
 public class ExternalStoragePermissionActivity extends Activity {
 
+    private static final int REQUEST_CODE_ENABLE_PERMISSION = 9090;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new View(this));
         requestPermission();
     }
 
@@ -26,35 +28,36 @@ public class ExternalStoragePermissionActivity extends Activity {
                 Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
                 intent.addCategory("android.intent.category.DEFAULT");
                 intent.setData(Uri.parse(String.format("package:%s", getApplicationContext().getPackageName())));
-                startActivityForResult(intent, 2296);
+                startActivityForResult(intent, REQUEST_CODE_ENABLE_PERMISSION);
             } catch (Exception e) {
                 Intent intent = new Intent();
                 intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
-                startActivityForResult(intent, 2296);
+                startActivityForResult(intent, REQUEST_CODE_ENABLE_PERMISSION);
             }
         } else {
-            JSONObject returnObj = new JSONObject();
-            ExternalStoragePermissions.addProperty(returnObj, ExternalStoragePermissions.KEY_ERROR, "MANAGE_APP_ALL_FILES_ACCESS_PERMISSION");
-            ExternalStoragePermissions.addProperty(returnObj, ExternalStoragePermissions.KEY_MESSAGE, "Unknown error.");
-            ExternalStoragePermissions.notifyPermissionsCallback(false, returnObj);
-            finish();
+            notifyError(new JSONObject());
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 2296) {
+        if (requestCode == REQUEST_CODE_ENABLE_PERMISSION) {
             JSONObject returnObj = new JSONObject();
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
                 ExternalStoragePermissions.addProperty(returnObj, ExternalStoragePermissions.KEY_RESULT_PERMISSION, Environment.isExternalStorageManager());
                 ExternalStoragePermissions.notifyPermissionsCallback(true, returnObj);
+                finish();
             } else {
-                ExternalStoragePermissions.addProperty(returnObj, ExternalStoragePermissions.KEY_ERROR, "MANAGE_APP_ALL_FILES_ACCESS_PERMISSION");
-                ExternalStoragePermissions.addProperty(returnObj, ExternalStoragePermissions.KEY_MESSAGE, "Unknown error.");
-                ExternalStoragePermissions.notifyPermissionsCallback(false, returnObj);
+                notifyError(returnObj);
             }
         }
+    }
+
+    private void notifyError(JSONObject jsonObject) {
+        ExternalStoragePermissions.addProperty(jsonObject, ExternalStoragePermissions.KEY_ERROR, "MANAGE_APP_ALL_FILES_ACCESS_PERMISSION");
+        ExternalStoragePermissions.addProperty(jsonObject, ExternalStoragePermissions.KEY_MESSAGE, "Unknown error.");
+        ExternalStoragePermissions.notifyPermissionsCallback(false, jsonObject);
         finish();
     }
 }
